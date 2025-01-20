b8f61a0926e88aeede48ed3036ddb310b1ecad47
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-01-20 02:07:46 -0500

    add link to repo

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -265,7 +265,7 @@ export default function Lab1() {
       <a href="https://www.lipsum.com" id="wd-lipsum">click here</a>
       to get dummy text<br/>
       Please
-      <a href="https://www.lipsum.com" id="wd-github">click here</a>
+      <a href="https://github.com/Anzhuo-W/kambaz-react-web-app" id="wd-github">click here</a>
       to go to github<br/>
     </div>
   );
