d14d032cd8b8236cfa960aed1f7a4963a9ee8a54
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-02-05 19:56:15 -0500

    update course status buttons

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -1,5 +1,11 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
-import { FaBan, FaCheckCircle } from "react-icons/fa";
+import { FaBan, FaBell, FaCheckCircle } from "react-icons/fa";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { BiImport } from "react-icons/bi";
+import { TiHome } from "react-icons/ti";
+import { VscGraph } from "react-icons/vsc";
+import { BsMegaphone } from "react-icons/bs";
+import { MdBarChart } from "react-icons/md";
 
 export default function CourseStatus() {
   return (
@@ -16,13 +22,27 @@ export default function CourseStatus() {
               <FaCheckCircle /> Publish</Button>
           </Col>
         </Row>
-        <Button variant="primary" className="w-100 text-start">Import Existing Content</Button>
-        <Button variant="primary" className="w-100 text-start">Import from Commons</Button>
-        <Button variant="primary" className="w-100 text-start">Choose Home Page</Button>
-        <Button variant="primary" className="w-100 text-start">View Course Stream</Button>
-        <Button variant="primary" className="w-100 text-start">New Announcement</Button>
-        <Button variant="primary" className="w-100 text-start">New Analytics</Button>
-        <Button variant="primary" className="w-100 text-start">View Course Notifications</Button>
+        <Button variant="primary" className="w-100 text-start">
+          <BiImport className="me-2 fs-5" />
+          Import Existing Content</Button>
+        <Button variant="primary" className="w-100 text-start">
+          <LiaFileImportSolid className="me-2 fs-5" />
+          Import from Commons</Button>
+        <Button variant="primary" className="w-100 text-start">
+          <TiHome className="me-2 fs-5"/>
+          Choose Home Page</Button>
+        <Button variant="primary" className="w-100 text-start">
+          <VscGraph className="me-2 fs-5"/>
+          View Course Stream</Button>
+        <Button variant="primary" className="w-100 text-start">
+          <BsMegaphone className="me-2 fs-5"/>
+          New Announcement</Button>
+        <Button variant="primary" className="w-100 text-start">
+          <MdBarChart className="me-2 fs-5"/>
+          New Analytics</Button>
+        <Button variant="primary" className="w-100 text-start">
+          <FaBell className="me-2 fs-5"/>
+          View Course Notifications</Button>
       </Container>
     </div>
   );
