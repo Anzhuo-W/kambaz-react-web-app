7e4cd6edd2d837692cf8187e92e6975a2f5ca417
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-02-18 01:43:49 -0500

    update navigation

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,34 +1,42 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useLocation } from "react-router";
 import CourseNavigation from "./Navigation.tsx";
 import Home from "./Home";
 import Modules from "./Modules";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor.tsx";
-import { GiHamburgerMenu } from "react-icons/gi";
 import PeopleTable from "./People/Table.tsx";
+import { useParams } from "react-router-dom";
+import { courses } from "../Database";
+import { FaAlignJustify } from "react-icons/fa";
 
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
   return (
-    <div>
-      <h2><GiHamburgerMenu /> Course Home</h2>
+    <div id="wd-courses">
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-3 fs-4 mb-1" />
+        {course && course.name} &gt; {pathname.split("/")[4]}
+      </h2>
       <hr />
       <table>
         <tbody>
-          <tr>
-            <td valign="top">
-              <CourseNavigation />
-            </td>
-            <td valign="top">
-              <Routes>
-                <Route path="/" element={<Navigate to="Home" />} />
-                <Route path="Home" element={<Home />} />
-                <Route path="Modules" element={<Modules />} />
-                <Route path="Assignments" element={<Assignments />} />
-                <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-                <Route path="People" element={<PeopleTable />} />
-              </Routes>
-            </td>
-          </tr>
+        <tr>
+          <td valign="top">
+            <CourseNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="Home" />} />
+              <Route path="Home" element={<Home />} />
+              <Route path="Modules" element={<Modules />} />
+              <Route path="Assignments" element={<Assignments />} />
+              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+              <Route path="People" element={<PeopleTable />} />
+            </Routes>
+          </td>
+        </tr>
         </tbody>
       </table>
     </div>
