031f318a337261219528439bbac6a96eb4572bbd
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-03-11 03:38:56 -0400

    update profile

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -1,56 +1,55 @@
-import { Link } from "react-router-dom";
-import { Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentUser } from "./reducer";
+import { KambazState } from "../store.ts";
+import { User } from "./Signin.tsx";
+import { Button, FormControl } from "react-bootstrap";
 
 export default function Profile() {
+  const [profile, setProfile] = useState<User>();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { currentUser } = useSelector((state: KambazState) => state.accountReducer);
+  const fetchProfile = () => {
+    if (!currentUser) return navigate("/Kambaz/Account/Signin");
+    setProfile(currentUser);
+  };
+  const signout = () => {
+    dispatch(setCurrentUser(null));
+    navigate("/Kambaz/Account/Signin");
+  };
+  useEffect(() => {
+    fetchProfile();
+  }, []);
   return (
-    <div id="wd-profile-screen" className="m-3">
-      <h1 className="mb-3">Profile</h1>
-      <Form.Control
-        defaultValue="alice"
-        placeholder="username"
-        className="wd-username mb-2"
-      />
-      <Form.Control
-        defaultValue="123"
-        placeholder="password"
-        type="password"
-        className="wd-password mb-2"
-      />
-      <Form.Control
-        defaultValue="Alice"
-        placeholder="First Name"
-        id="wd-firstname"
-        className="mb-2"
-      />
-      <Form.Control
-        defaultValue="Wonderland"
-        placeholder="Last Name"
-        id="wd-lastname"
-        className="mb-2"
-      />
-      <Form.Control
-        type="date"
-        id="wd-dob"
-        className="mb-2"
-      />
-      <Form.Control
-        defaultValue="alice@wonderland.com"
-        type="email"
-        id="wd-email"
-        className="mb-2"
-      />
-      <Form.Select defaultValue="USER" id="wd-role" className="mb-2">
-        <option value="USER">User</option>
-        <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option>
-        <option value="STUDENT">Student</option>
-      </Form.Select>
-      <Link
-        to="/Kambaz/Account/Signin"
-        className="btn btn-danger text-white w-100 mb-2"
-      >
-        Sign out
-      </Link>
-    </div>
-  );
+    <div className="wd-profile-screen m-3">
+      <h3 className="mb-3">Profile</h3>
+      {profile && (
+        <div>
+          <FormControl defaultValue={profile.username} id="wd-username" className="mb-2"
+                       onChange={(e) => setProfile({ ...profile, username: e.target.value })} />
+          <FormControl defaultValue={profile.password} id="wd-password" className="mb-2"
+                       onChange={(e) => setProfile({ ...profile, password: e.target.value })} />
+          <FormControl defaultValue={profile.firstName} id="wd-firstname" className="mb-2"
+                       onChange={(e) => setProfile({ ...profile, firstName: e.target.value })} />
+          <FormControl defaultValue={profile.lastName} id="wd-lastname" className="mb-2"
+                       onChange={(e) => setProfile({ ...profile, lastName: e.target.value })} />
+          <FormControl defaultValue={profile.dob} id="wd-dob" className="mb-2"
+                       onChange={(e) => setProfile({ ...profile, dob: e.target.value })} type="date" />
+          <FormControl defaultValue={profile.email} id="wd-email" className="mb-2"
+                       onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
+          <select onChange={(e) => setProfile({ ...profile, role: e.target.value })}
+                  className="form-control mb-2" id="wd-role">
+            <option value="USER">User</option>
+            <option value="ADMIN">Admin</option>
+            <option value="FACULTY">Faculty</option>
+            <option value="STUDENT">Student</option>
+          </select>
+          <Button onClick={signout} className="w-100 mb-2" id="wd-signout-btn">
+            Sign out
+          </Button>
+        </div>
+      )}
+    </div>);
 }
--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -3,8 +3,11 @@ import Signup from "./Signup.tsx";
 import Profile from "./Profile.tsx";
 import { Navigate, Route, Routes } from "react-router";
 import AccountNavigation from "./Navigation.tsx";
+import { useSelector } from "react-redux";
+import { KambazState } from "../store.ts";
 
 export default function Account() {
+  const { currentUser } = useSelector((state: KambazState) => state.accountReducer);
   return (
     <div id="wd-account-screen">
       <table>
@@ -17,7 +20,7 @@ export default function Account() {
             <Routes>
               <Route
                 path="/"
-                element={<Navigate to="/Kambaz/Account/Signin" />}
+                element={<Navigate to={currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin"} />}
               />
               <Route path="/Signin" element={<Signin />} />
               <Route path="/Profile" element={<Profile />} />
