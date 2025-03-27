ea938f27d838d6c23cf85b252e3e5ca604efd4cc
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-03-26 22:52:05 -0400

    add credentials

--- src/Kambaz/Courses/Assignments/client.ts
+++ src/Kambaz/Courses/Assignments/client.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { Assignment } from "./reducer.ts";
 
+const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 
 export const createAssignment = async (courseId: string, assignment: Assignment) => {
-  const { data } = await axios.post(
+  const { data } = await axiosWithCredentials.post(
     `${COURSES_API}/${courseId}/assignments`,
     assignment
   );
@@ -14,17 +15,17 @@ export const createAssignment = async (courseId: string, assignment: Assignment)
 };
 
 export const findAssignmentsForCourse = async (courseId: string) => {
-  const { data } = await axios.get(`${COURSES_API}/${courseId}/assignments`);
+  const { data } = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/assignments`);
   return data;
 };
 
 export const findAssignmentById = async (assignmentId: string) => {
-  const { data } = await axios.get(`${ASSIGNMENTS_API}/${assignmentId}`);
+  const { data } = await axiosWithCredentials.get(`${ASSIGNMENTS_API}/${assignmentId}`);
   return data;
 };
 
 export const updateAssignment = async (assignment: Assignment) => {
-  const { data } = await axios.put(
+  const { data } = await axiosWithCredentials.put(
     `${ASSIGNMENTS_API}/${assignment._id}`,
     assignment
   );
@@ -32,6 +33,6 @@ export const updateAssignment = async (assignment: Assignment) => {
 };
 
 export const deleteAssignment = async (assignmentId: string) => {
-  const { data } = await axios.delete(`${ASSIGNMENTS_API}/${assignmentId}`);
+  const { data } = await axiosWithCredentials.delete(`${ASSIGNMENTS_API}/${assignmentId}`);
   return data;
 };
